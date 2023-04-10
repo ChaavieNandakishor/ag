a7df04dd4df9dc1ba8216cefa6aa4f5863d5d33a
ChaavieNandakishor <chaavienandakishor@gmail.com>
2023-04-10 23:19:37 +0530

    first commit

---
+++ src/Ag.jsx
@@ -0,0 +1,73 @@
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useMemo,
+  useCallback,
+} from "react";
+import { render } from "react-dom";
+import { AgGridReact } from "ag-grid-react"; // the AG Grid React Component
+
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
+
+export default function Ag() {
+  const gridRef = useRef(); // Optional - for accessing Grid's API
+  const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
+
+  // Each Column Definition results in one Column.
+  const [columnDefs, setColumnDefs] = useState([
+    { field: "Name"},
+    { field: "Age"},
+    { field: "Class" },
+  ]);
+
+  // DefaultColDef sets props common to all Columns
+  const defaultColDef = useMemo(() => ({
+    sortable: true,
+    filter:true
+  }));
+
+  // Example of consuming Grid Event
+  const cellClickedListener = useCallback((event) => {
+    console.log("cellClicked", event);
+  }, []);
+
+  const ab=[
+  {Name:"amal",Age:"23",Class:"Bca"},
+  {Name:"binoy",Age:"22",Class:"Bca"},
+  {Name:"manu",Age:"21",Class:"Bca"},
+ 
+  ]
+
+  // Example load data from server
+  useEffect(() => {
+    fetch("https://www.ag-grid.com/example-assets/row-data.json")
+      .then((result) => result.json())
+      .then((rowData) => setRowData(rowData));
+  }, []);
+
+  // Example using Grid's API
+  const buttonListener = useCallback((e) => {
+    gridRef.current.api.deselectAll();
+  }, []);
+  return (
+    <div>
+      {/* Example using Grid's API */}
+      <button onClick={buttonListener}>Push Me</button>
+
+      {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
+      <div className="ag-theme-alpine" style={{ width: 900, height: 200 }}>
+        <AgGridReact
+          ref={gridRef} // Ref for accessing Grid's API
+          rowData={ab} // Row Data for Rows
+          columnDefs={columnDefs} // Column Defs for Columns
+          defaultColDef={defaultColDef} // Default Column Properties
+          animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+          rowSelection="multiple" // Options - allows click selection of rows
+          onCellClicked={cellClickedListener} // Optional - registering for Grid Event
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Ag from './Ag';
+
+function App() {
+  return (
+    <div>
+      <Ag/>
+
+     
+    </div>
+  );
+}
+
+export default App;
